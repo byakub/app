d5ed589abdc5b7e6a6cfdedf6d89dfe4238ccb0f
Bohdan <steamfirst@ukr.net>
2020-12-15 13:39:53 +0200

    Added API client service.

--- src/containers/Root/Root.tsx
+++ src/containers/Root/Root.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router";
 
 import { RouteConst } from "consts";
+import Components from "components";
 
 export const Root: React.FC = () => {
   return (
     <>
       <Switch>
+        <Route path="/" component={Components} />
         <Route path={RouteConst.CharactersList} component={Characters} />
         <Redirect to={RouteConst.Root} />
       </Switch>
